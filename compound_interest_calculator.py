b818e61b198649b8866dd55be608fc3230c187ba
Jacob DeWitt <32183923+QuantumHacker0@users.noreply.github.com>
2024-05-11 11:39:40 -0500

    Update compound_interest_calculator.py
    Fixed the formula by adding "n" to exponents and cleaning up the order of operations.

--- compound_interest_calculator.py
+++ compound_interest_calculator.py
@@ -2,15 +2,15 @@ import math
 
 amount = None
 principal = None
-intrestRate = None
-intrestCompYearly = None
+interestRate = None
+interestCompYearly = None
 years = None
 
 principal = input("What is your principal amount? ")
-intrestRate = input("What is the intrest rate? ")
-intrestCompYearly = input("How often is intrest calculated? ")
+interestRate = input("What is the interest rate? ")
+interestCompYearly = input("How often is interest calculated? ")
 years = input("How many years will your account be active? ")
 
-amount = float(principal)*(1 + (float(intrestRate)/float(intrestCompYearly))) ** float(years)
+amount = float(principal)*(1 + (float(interestRate)/float(interestCompYearly))) ** (float(interestCompYearly) * float(years))
 
 print(round(amount, 2))
